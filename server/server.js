4fcdea14f01a9ef0dacb3e0f5a4abc4ff493436e
Abhay kumar <106826721+absiemon@users.noreply.github.com>
2023-12-18 04:23:45 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json({
 app.use(morgan());
 app.disable('etag')
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['https://time-sync.onrender.com'];
 const corsOptions = {
     credentials: true,
     origin: allowedOrigins,
